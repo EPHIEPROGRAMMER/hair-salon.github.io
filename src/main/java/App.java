2f246d62642d90ea727480a07769e3d0aa0df079
ephie <ephie@localhost.localdomain>
2017-08-25 15:52:25 +0300

    the route renders stylist-clients-form that links the stylists to the clients

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -60,6 +60,15 @@ public class App {
     return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    // relating stylist to the clients
+    get("/stylists/:id/clients/new", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+        model.put("stylist", stylist);
+        model.put("template", "templates/stylist-clients-form.vtl");
+        return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
 
   }
 }
